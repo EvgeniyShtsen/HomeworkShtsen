29f96b6eee7fe9f7b06acf6cc1382117c4d04a17
EvgeniyShtsen <grolly42@mail.ru>
2022-06-01 22:03:43 +0300

    Add project files.

---
+++ Homework/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Days
+{
+    class Program
+    {
+        static void OperationNumber(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Max value: 7");
+                    break;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Please, enter a day number");
+            var number = 0;
+            var isNumber = int.TryParse(Console.ReadLine(), out number);
+            if (isNumber)
+            {
+                OperationNumber(number);
+            }
+            else
+            {
+                Console.WriteLine("Please, enter an integer and try again");
+            }
+        }
+    }
+}
